2dbb168308df03c041ec89f0dff73e34339fe4f8
Alessandro Silvestri <alessandro.silvestri.work@gmail.com>
2023-02-06 07:53:49 +0000

    complete class template

--- atm.py
+++ atm.py
@@ -5,3 +5,20 @@ Withdrawing cash, Depositing money, Balance, quit
 class Atm():
     def __init__(self):
         self.balance = 2000
+
+    def show_balance(self):
+        print(self.balance)
+    
+    def deposit(self, n):
+        self.balance += n
+    
+    def withdraw(self, n):
+        self.balance -= n
+    
+
+atm = Atm()
+atm.show_balance()
+atm.deposit(500)
+atm.show_balance()
+atm.withdraw(100)
+atm.show_balance()
