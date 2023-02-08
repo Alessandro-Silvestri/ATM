a2cf0386ea8c9076db6e42e91abb859d8f8e3562
Alessandro Silvestri <alessandro.silvestri.work@gmail.com>
2023-02-08 01:02:52 +0000

    update all the sections and they works without errors

--- atm.py
+++ atm.py
@@ -7,6 +7,7 @@ os.system('clear')
 class Atm():
     def __init__(self, balance):
         self.balance = balance
+        self.withdraw_choice = ["1", "2", "3", "4", "5", "6", "7"]
         self.withdraw_nums = {
             "1": 10,
             "2": 20,
@@ -24,12 +25,7 @@ class Atm():
         self.balance += n
     
     def withdraw(self, n):
-        if n > self.balance:
-            print(f"the maximum withdraw is: {self.balance}")
-        elif n <= 0:
-            print("It's not possible to insert 0 ora a negative number")
-        else:
-            self.balance -= n
+        self.balance -= n
 
     def home_screen(self):
         print('''
@@ -59,18 +55,40 @@ atm = Atm(1500)
 atm.home_screen()
 user_input = input("> ")
 
-if user_input == "1": 
-    # withdraw
+if user_input == "1": # Choice 1: withdraw
     os.system('clear')
     atm.home_withdraw()
     user_input = input("> ")
-    atm.balance -= atm.withdraw_nums.get((user_input))
+    
+    if user_input in atm.withdraw_choice:
+        atm.withdraw(atm.withdraw_nums.get((user_input)))
+        atm.view_balance()
+   
+    elif user_input == "8":
+        print('other choice')
+        while True:
+            # I ask the amount and I check if it is more than the funds
+            user_input = int(input('write the amount: '))
+            if user_input > atm.balance:
+                print('Not sufficient funds')
+                continue
+            elif user_input == 0 or user_input < 0:
+                print("wrong input")
+                continue
+            else:
+                atm.withdraw(user_input)
+                atm.view_balance()
+                break
+    else:
+        print("wrong choice")
+    
+elif user_input == "2": # Choice 2: deposit
+    user_input = int(input('Deposit: write the amount: '))
+    atm.deposit(user_input)
     atm.view_balance()
 
-    
-elif user_input == "2":
-    pass
-elif user_input == "3":
+elif user_input == "3": # Choice 3: view balance
     atm.view_balance()
-elif user_input == "4":
-    pass
+
+elif user_input == "4": # Choice 4: quit
+    print('Thank you, bye')
